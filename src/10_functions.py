e8a60e318070349c7cb85ad8f2370dc972b11f1a
azrap <azraap@gmail.com>
2019-10-28 12:09:39 -0700

    fixed some formatting using the pep8 linter

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,7 +1,8 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
+
 def is_even(x):
-    if x%2==0:
+    if x % 2 == 0:
         return True
     else:
         return False
@@ -12,25 +13,20 @@ print(is_even(3))
 
 
 # Read a number from the keyboard
-
 # x='hello'
 # print(x.isnumeric())
-
-
-
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
 def my_function():
-    x='string'
-    while x.isnumeric()==False:
-        x=input("please input a number here:")
-    if x.isnumeric()==True:
-        if float(x)%2==0:
+    x = 'string'
+    while x.isnumeric() is False:
+        x = input("please input a number:")
+    if x.isnumeric() is True:
+        if float(x) % 2 == 0:
             print("Even!")
         else:
             print("Odd")
-        
 
 
 my_function()
